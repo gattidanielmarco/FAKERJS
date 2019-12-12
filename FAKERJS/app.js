48be9525bd9382d77763afbc24e08681d0322b2b
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2019-12-12 21:17:47 +0000

    non viene

--- FAKERJS/app.js
+++ FAKERJS/app.js
@@ -12,7 +12,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.get('/', function (req, res) {
+ //res.send('Ciao Mondo');
+ res.render('index');  //Dico a express di processare e inviare la pagina index.pug
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
